281477f3a8ee51e9426e425f1717d827f70adf19
Ratul Roy <66505807+royal-programmer@users.noreply.github.com>
2024-07-09 23:46:57 +0530

    Delete server directory

--- server/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const connectDatabase = () => {
-  mongoose.connect(process.env.DB_LOCAL_URI, {}).then(_0x3c1038 => {
-    console.log("MongoDB Database connected with HOST: " + _0x3c1038.connection.host);
-  });
-};
-module.exports = connectDatabase;
